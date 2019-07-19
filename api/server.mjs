b3c519e3d1fb0f3f83a254793693817cbebc8147
Joaquin Moldes <jmoldes@enantar.es>
2019-07-19 10:49:40 +0200

    Update api for use express and redis

--- api/server.mjs
+++ api/server.mjs
@@ -1,10 +1,42 @@
-import apolloServerKoa from 'apollo-server-koa'
-import Koa from 'koa'
+import apolloServerExpress from 'apollo-server-express'
+import express from 'express'
 import resolvers from './resolvers'
 import typeDefs from './types'
 
-const app = new Koa()
-const server = new apolloServerKoa.ApolloServer({
+import cookieParser from 'cookie-parser'
+import ConnectRedisSessions from 'connect-redis-sessions'
+
+const app = express()
+
+const fnCRS = ConnectRedisSessions({
+  app: 'apollo-upload',  // A simple string as appname or a function to calc the name on every request.
+  ttl: 1000000, // Redis session timeout to wipe the session on idle time
+  cookie: { // Cookie configuration. If nothing is set a browser session cookie will be used
+    maxAge: 10 * 60*60*24 * 1000,
+    httpOnly: true
+  }
+})
+
+app.use(cookieParser())
+
+app.use(fnCRS)
+
+// middleware to simulate a session redis
+app.use((req, res, next) => {
+  if (!req.session.id) {
+    // The first time a session is created in redis and a cookie is sent to the client
+    req.session.upgrade('user', 60*60*24 * 10, () => {
+      req.session.idApp= 3000
+      req.session.idUser = 1
+      req.session.email = 'userName@gmail.com'
+      req.session.nombre = 'UserName'
+    })
+    console.log('Created Session')
+  }
+  next() 
+})
+
+const server = new apolloServerExpress.ApolloServer({
   typeDefs,
   resolvers,
   uploads: {
